3c208e12ce8e25a9f2e238fd59aea35f908aaa91
Pampapathi K <pampapathi@kalajitha.com>
2024-11-24 15:08:15 +0530

    Add project files.

---
+++ webapi/Data/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapi.Models;
+
+namespace webapi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+       public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+       { 
+
+
+       }
+        public DbSet<Stock> Stock {  get; set; }
+        public DbSet<Comments> Comments { get; set; }
+
+        // search the individual data tables from the database
+
+
+    }
+}
+
+// ORM -> transfer database tables into object
